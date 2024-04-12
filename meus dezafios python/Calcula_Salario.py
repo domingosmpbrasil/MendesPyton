62c13a1bcf6af2f7b3eff78939988e9c2b3e729e
Rogério Rodrigues Silva <114150848+RogerioRSilva@users.noreply.github.com>
2024-04-12 19:35:32 -0300

    Apontamentos para correção e sugestões

--- meus dezafios python/Calcula_Salario.py
+++ meus dezafios python/Calcula_Salario.py
@@ -15,7 +15,10 @@
 
 
 ganho_por_hora = float(input("Digite aqui quanto você ganha por hora: "))
+# Remover a conversão de numeros pois não há necessidade sendo que o python já reconhece e adere o tipo do valor que vc atribui para a variável. 
 horas_por_dia = int(8)
+# Sugesstão: Sabendo que os valores serão calculados em porcentagem vc precisa fazer a conversão, sendo assim pode fazer a conversão dentro da propria variável dividindo o valor por 100.
+# Somente fará isso se estiver trabalhando com porcentagem OK. IR, INSS e SINDICATO são porcentagens.  
 desconto_ir = float(11)
 desconto_inss = float(8)
 desconto_sindicato = float(5)
@@ -27,7 +30,7 @@ valor_inss = float(desconto_inss * salario_bruto) / 100
 valor_sindicato = float(desconto_sindicato * salario_bruto) / 100
 valor_salario_liquido = float(salario_bruto - valor_do_ir - valor_inss - valor_sindicato)
 
-
+# Sugestão: Casos queira para ficar com um visual melhor pode definir um print para cada campo. Dessa forma a linha de retorno fica muito grande e visualmente ruim pra poder enxergar OK.  
 print(f'Seu salário bruto mensal é de: {salario_bruto}, ------ você vai pagar de imposto de renda: {valor_do_ir}, ------ o valor descontado do INSS é de: {valor_inss}, ------ o desconto do sindicato é de: {valor_sindicato}, ------ Seu salário líquido é de: {valor_salario_liquido:.2f}')
 
 
